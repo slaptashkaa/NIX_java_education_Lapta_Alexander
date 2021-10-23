79b252c00652cc5e5e5a29f827377e9da7a9d81e
slaptashka <o.o.lapta@student.khai.edu>
2021-10-23 14:48:24 +0300

    ChatBot 1-st stage

---
+++ src/nix/education/java/chatbot/ChatBot.java
@@ -0,0 +1,11 @@
+package nix.education.java.chatbot;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        String bot_name = "Aboba";
+        int birth_year = 2021;
+
+        System.out.printf("Hello, my name is %s\n" +
+                "I was created in %d", bot_name, birth_year);
+    }
+}
