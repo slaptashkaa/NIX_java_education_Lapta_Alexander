6fed82fb4d00c3e502ac2e73f8afd427d02993bd
slaptashka <o.o.lapta@student.khai.edu>
2021-10-23 14:53:51 +0300

    ChatBot 2-nd stage

--- src/nix/education/java/chatbot/ChatBot.java
+++ src/nix/education/java/chatbot/ChatBot.java
@@ -1,11 +1,20 @@
 package nix.education.java.chatbot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
-        String bot_name = "Aboba";
-        int birth_year = 2021;
+        final String bot_name = "Aboba";
+        final int birth_year = 2021;
 
         System.out.printf("Hello, my name is %s\n" +
-                "I was created in %d", bot_name, birth_year);
+                "I was created in %d\n", bot_name, birth_year);
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please, remind me your name.");
+
+        String your_name = scan.nextLine();
+        System.out.printf("What a great name you have, %s!", your_name);
     }
 }
